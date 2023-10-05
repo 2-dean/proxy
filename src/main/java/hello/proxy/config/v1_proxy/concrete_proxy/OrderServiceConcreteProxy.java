ee9af41ed3b51b995cf54c3a2a93c4b3ea84df44
2-dean <limsjs@naver.com>
2023-10-05 15:31:02 +0900

    section 4 end

---
+++ src/main/java/hello/proxy/config/v1_proxy/concrete_proxy/OrderServiceConcreteProxy.java
@@ -0,0 +1,19 @@
+package hello.proxy.config.v1_proxy.concrete_proxy;
+
+import hello.proxy.app.v2.OrderRepositoryV2;
+import hello.proxy.app.v2.OrderServiceV2;
+import hello.proxy.trace.logtrace.LogTrace;
+
+public class OrderServiceConcreteProxy extends OrderServiceV2 {
+
+    private final OrderServiceV2 target;
+    private final LogTrace logTrace;
+
+    public OrderServiceConcreteProxy(OrderServiceV2 target, LogTrace logTrace) {
+        super(null); // 부모 객체의 기느을 사용하지 않기때문에 null
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+
+}
